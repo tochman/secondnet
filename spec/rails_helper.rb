a171e22cdb31a82803e49d620a01e8100dc8e65f
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-11-11 16:40:15 +0100

    added coveralls gem and added travis and coverall badges to readme

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'coveralls'
+Coveralls.wear!
 
 ActiveRecord::Migration.maintain_test_schema!
 
